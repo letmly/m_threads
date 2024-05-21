7e12979fe9492ec3184a1541e02b81c63445db9a
letmly <102484956+letmly@users.noreply.github.com>
2024-05-21 20:19:00 +0300

    4 threads segmentation
    типо 4 треда делают свою ветку

--- mt4/bsp.cpp
+++ mt4/bsp.cpp
@@ -97,38 +97,6 @@ BSPNode* buildBSP(v_seg& segments, int depth = 0) {
     return node;
 }
 
-
-BSPNode* mt_buildBSP(vector<Segment>& segments, int depth = 0) {
-    if (segments.empty()) return nullptr;
-    if (segments.size() == 1) {
-        auto node = new BSPNode(segments);
-        return node;
-    }
-    Segment div_seg = segments[segments.size() / 2];
-    v_seg frontSet, backSet, intersectSet;
-
-    divideSet(segments, div_seg, frontSet, backSet, intersectSet);
-    auto node = new BSPNode(intersectSet);
-
-    if (depth == 0) {
-        #pragma omp task shared(node)
-        {
-            node->back = buildBSP(backSet, depth + 1);
-        }
-        #pragma omp task shared(node)
-        {
-            node->front = buildBSP(frontSet, depth + 1);
-        }
-
-        #pragma omp taskwait
-    } else {
-        node->back = buildBSP(backSet, depth + 1);
-        node->front = buildBSP(frontSet,depth + 1);
-    }
-
-    return node;
-}
-
 v_seg generateSegments(int& count) {
     v_seg segments;
     mt19937 rng(random_device{}());
@@ -154,8 +122,10 @@ void mt4_test(int s_count = 10, bool write_tree = false, std::string from = "", 
     cout << "worst predict of segments amount = " << s_count + 4 * s_count * log2(s_count) << "\n";
     cout << "good predict of segments amount = " << s_count + 2 * s_count * log2(s_count) << "\n";
 
-    BSPNode* root = nullptr;
+
     for (int i : {1, 2, 3, 1652334, 9978595, 536262334, 88878595}) {
+        BSPNode* root = nullptr;
+
         segments = generateSegments(s_count);
         std::minstd_rand0 g1 (i);
         std::shuffle(std::begin(segments), std::end(segments), g1);
@@ -165,20 +135,55 @@ void mt4_test(int s_count = 10, bool write_tree = false, std::string from = "", 
         double st = omp_get_wtime();
         root = buildBSP(segments_copy);
         double ft = omp_get_wtime();
+        cout << "\n\n-----------------TEST-----------------\n";
         cout << "\tRACE BSP built of " << s_count << "segs in: " << ft - st << " seconds.\n";
-//        cout << "\tRACE BSP seg count: " << root->countSegments() << "\n\n";
+        cout << "\tRACE BSP seg count: " << root->countSegments() << "\n\n";
 
         segments_copy = segments;
 
-        BSPNode* mt_root = nullptr;
         st = omp_get_wtime();
         omp_set_max_active_levels(10);
+        Segment div_seg = segments[segments.size() / 2];
+        v_seg frontSet, backSet, intersectSet;
 
-        mt_root = mt_buildBSP(segments_copy);
+        divideSet(segments, div_seg, frontSet, backSet, intersectSet);
+        auto mt_root = new BSPNode(intersectSet);
+
+        v_seg frontSet1, backSet1, intersectSet1;
+        div_seg = frontSet[frontSet.size() / 2];
+        divideSet(frontSet, div_seg, frontSet1, backSet1, intersectSet1);
+        auto sub_node1 = new BSPNode(intersectSet1);
+
+        v_seg frontSet2, backSet2, intersectSet2;
+        div_seg = backSet[backSet.size() / 2];
+        divideSet(backSet, div_seg, frontSet2, backSet2, intersectSet2);
+        auto sub_node2 = new BSPNode(intersectSet2);
 
 
-        ft = omp_get_wtime();
-        cout << "\tMultiProc BSP built of " << s_count << "segs in: " << ft - st << " seconds.\n";
+        #pragma omp parallel num_threads(4)
+        {
+            #pragma omp single
+            {
+                sub_node1->front = buildBSP(frontSet1);
+            }
+            #pragma omp single
+            {
+                sub_node1->back = buildBSP(backSet1);
+            }
+            #pragma omp single
+            {
+                sub_node2->front = buildBSP(frontSet2);
+            }
+            #pragma omp single
+            {
+                sub_node2->back = buildBSP(backSet2);
+            }
+        }
+
+        mt_root->front = sub_node1;
+        mt_root->back = sub_node2;
+
+        cout << "\tMultiProc BSP built of " << s_count << "segs in: " << omp_get_wtime() - st << " seconds.\n";
         cout << "\tMultiProc BSP seg count: " << mt_root->countSegments() << "\n";
     }
 }
@@ -186,29 +191,24 @@ void mt4_test(int s_count = 10, bool write_tree = false, std::string from = "", 
 void mt4_test_hard(string spec_dir = "out") {
 
     vector<int> segments_counts = { 100, 1000, 10000};
+    double st;
+    v_seg segments_copy;
 
+    BSPNode* root = nullptr;
+    BSPNode* mt_root = nullptr;
     for (int& count : segments_counts) {
         v_seg segments = generateSegments(count);
-        v_seg segments_copy(segments);
+        segments_copy = segments;
 
-        BSPNode* root = nullptr;
         double st = omp_get_wtime();
-        root = buildBSP(segments);
-        double ft = omp_get_wtime();
-        cout << "\tRACE BSP built of " << count << "segs in: " << ft - st << "seconds.\n";
+        root = buildBSP(segments_copy);
 
         BSPNode* mt_root = nullptr;
         st = omp_get_wtime();
         mt_root = buildBSP(segments_copy);
-        ft = omp_get_wtime();
-        cout << "\tMultiProc BSP built of " << count << "segs in: " << ft - st << " seconds.\n";
+        cout << "\tMultiProc BSP built of " << count << "segs in: " << omp_get_wtime() - st << " seconds.\n";
 
         cout << "saving trees...\n";
 
-        if (*mt_root == *root) {
-            std::cout << "BSP trees are equal." << std::endl;
-        } else {
-            std::cout << "TREES ARE DIFFERENT!." << std::endl;
-        }
     }
 }
